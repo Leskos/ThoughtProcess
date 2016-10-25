3cc898f9573e983c28602ceb4cea2282c491235c
Louis d'Aboville <louisdaboville@hotmail.com>
2016-10-25 23:06:31 +0100

    Added ThoughtProcess_TestScene

---
+++ Assets/_scripts/ConstantRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantRotation : MonoBehaviour 
+{
+	public Vector3 rotationsPerMinute = Vector3.zero;
+
+	void Update () 
+	{
+		transform.Rotate ( rotationsPerMinute.x*6.0f*Time.deltaTime, rotationsPerMinute.y*6.0f*Time.deltaTime, rotationsPerMinute.z*6.0f*Time.deltaTime );
+	}
+}
