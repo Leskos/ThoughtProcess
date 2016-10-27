6070fad6c30096969a0d69a7c84247f1f9f29f65
Louis d'Aboville <louisdaboville@hotmail.com>
2016-10-28 00:01:34 +0100

    Prepared all 8 panels for content

---
+++ Assets/HideFrame.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using SVGImporter;
+
+public class HideFrame : MonoBehaviour 
+{
+
+	private SVGRenderer svgRenderer;
+	public  Transform playerTranform;
+
+	// Use this for initialization
+	void Awake() {
+		svgRenderer = GetComponent<SVGRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerTranform.position.z > transform.position.z+0.01) {
+			svgRenderer.enabled = false;
+		} else {
+			svgRenderer.enabled = true;
+		}
+	}
+}
--- Assets/_scripts/PortalDebugger.cs
+++ Assets/_scripts/PortalDebugger.cs
@@ -1,18 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+
+#if UNITY_EDITOR
 using UnityEditor;
 
 [ExecuteInEditMode]
+#endif
+
 public class PortalDebugger : MonoBehaviour 
 {
-	
+	#if UNITY_EDITOR
 
-	public bool       active = false;
-	[Range(1,2)]
+	public bool        active = false;
 	public int         layerNumber;
 	public LayerInfo[] layerInfo;
-	public GameObject quad;
-	private int prevLayerNumber;
+	public GameObject  quad;
+
+	private int        prevLayerNumber;
+	private bool       prevActive;
 
 	void OnEnable()
 	{
@@ -27,40 +32,59 @@ public class PortalDebugger : MonoBehaviour
 
 	void PositionPortal()
 	{
-		
-		if ( active ) {
-			if (layerNumber <= layerInfo.Length) {
-				quad.SetActive (true);
 
-				if( prevLayerNumber != layerNumber ) {
-					SwitchLayer ();
-				}
+		// Re-enable all layers when deactivating
+		if( prevActive && !active ) { 
+			Debug.Log ( "Portal Debugger : Reactivating all layers" );
+			foreach ( LayerInfo layer in layerInfo ) {
+				Tools.lockedLayers  &= ~(1 << LayerMask.NameToLayer ( layer.layerName )); // Unlock all portal layers
+				Tools.visibleLayers |=   1 << LayerMask.NameToLayer ( layer.layerName );  // Show all portal layers
+			}
+		}
+
+		// Re-solo layer when reactiveating
+		if( !prevActive && active ) { 
+			Debug.Log ( "Portal Debugger : Re-soloing layer" );
+			SoloLayer ();
+		}
+		prevActive = active;
+
+		if ( active ) {
+			if ( layerNumber > 0 && layerNumber <= layerInfo.Length ) {
+				
+				// Switch layers if needed
+				if( prevLayerNumber != layerNumber ) { SoloLayer (); }
 				prevLayerNumber = layerNumber;
 
+				// Reposition quad to match scene view
+				quad.SetActive (true);
 				if (SceneView.lastActiveSceneView != null) {					
 					transform.position = SceneView.lastActiveSceneView.camera.transform.position;
 					transform.rotation = SceneView.lastActiveSceneView.camera.transform.rotation;
 				}		
 			} else {
-				Debug.Log ( "Invalid layer index" );
+				Debug.Log ( "Invalid layer index : " + layerNumber );
 			}
 
 		} else {
 			quad.SetActive (false);
 		}
-
 	}
 
-	private void SwitchLayer()
+
+	private void SoloLayer()
 	{
+		Debug.Log ( "Portal Debugger : Soloing layer" );
 		quad.GetComponent<MeshRenderer> ().material = layerInfo[layerNumber - 1].layerMaterial;
 
-		foreach (LayerInfo layer in layerInfo) {
-			Tools.lockedLayers |= 1 << LayerMask.NameToLayer (layer.layerName); // Add all portal layers to locked layers
+		foreach ( LayerInfo layer in layerInfo ) {
+			Tools.lockedLayers  |= 1 << LayerMask.NameToLayer (    layer.layerName );  // Lock all portal layers
+			Tools.visibleLayers  &= ~(1 << LayerMask.NameToLayer ( layer.layerName )); // Hide all portal layers
 		}
-
-		Tools.lockedLayers &= ~(1 << LayerMask.NameToLayer ( layerInfo[layerNumber - 1].layerName )); // Add all portal layers to locked layers
+		Tools.lockedLayers  &= ~(1 << LayerMask.NameToLayer ( layerInfo[layerNumber - 1].layerName )); // Unlock selected portal layer
+		Tools.visibleLayers |=   1 << LayerMask.NameToLayer ( layerInfo[layerNumber - 1].layerName );  // Show selected portal layer
 	}
+	#endif
 }
 
 
